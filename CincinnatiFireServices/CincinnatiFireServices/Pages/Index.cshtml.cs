6f6abf61bf3e429802cca1d89aedcdf98e62cdfe
Shruthi Ramesh <shruthi194.ramesh@gmail.com>
2021-03-26 21:47:33 -0400

    Index file commit

--- CincinnatiFireServices/CincinnatiFireServices/Pages/Index.cshtml.cs
+++ CincinnatiFireServices/CincinnatiFireServices/Pages/Index.cshtml.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using QuickType;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace CincinnatiFireServices.Pages
@@ -19,7 +24,29 @@ namespace CincinnatiFireServices.Pages
 
         public void OnGet()
         {
-
+            using (var webClient = new WebClient())
+            {
+                string jsonString = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/vnsz-a3wp.json");
+                JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("incidentjsonschema.json"));
+                JArray jsonArray = JArray.Parse(jsonString);
+                IList<string> validationEvents = new List<string>();
+                if(jsonArray.IsValid(schema))
+                {
+                
+                    var incident = Incident.FromJson(jsonString);
+                    ViewData["Incidents"] = incident;
+                }
+                else
+                {
+                    foreach(string evt in validationEvents)
+                    {
+                        Console.WriteLine(evt);
+                    }
+                    ViewData["Incidents"] = new List<Incident>();
+                }
+                
+             }
+        
         }
     }
 }
